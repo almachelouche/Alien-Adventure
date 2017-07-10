7e48cc7082dab70bdb2b501ea311fdc98334fda8
Alma Chelouche <alma.chelouche@gmail.com>
2017-07-10 16:06:49 +0300

    Initial Commit

---
+++ Alien Adventure/ReverseLongestName.swift
@@ -0,0 +1,25 @@
+//
+//  ReverseLongestName.swift
+//  Alien Adventure
+//
+//  Created by Jarrod Parkes on 9/30/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+extension Hero {
+    
+    func reverseLongestName(inventory: [UDItem]) -> String {
+        
+        var longestName = ""
+        for item in inventory {
+            if name!.characters.count>longestName.characters.count {
+            longestName = String(item.name.characters.reversed())
+            }
+        }
+    return longestName
+        }
+    
+}
+
+
+// If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
